2a9c94ed7421d78d10dd89b28f21004e8aa9a2f4
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-06-22 07:39:29 +0900

    G task is done

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,11 @@ const currentTime = moment().format("YYYY MM DD");
 console.log(currentTime);
 const person: string = "Martin"
 const count: number = 100;*/
+
+// Architectural pattern(suyak, malumotlar oqimini tartibga soluvchi vosita): 
+// MVC = MODEL VIEW CONTROLLER
+// DI = DEPENDENCY INJECTION
+// MVP
+
+// Design pattern(vazifa bajaruvchi qismi, butun backend tizimini malum bulaklarini struktutarasini yechishga xizmat qiladigan pattern): 
+// Middleware, Decorater
---
+++ src/train.ts
@@ -0,0 +1,19 @@
+// G task
+
+function getHighestIndex(arr: number[]): number{
+    if(arr.length === 0) return -1
+
+    let maxVal: number = arr[0];
+    let maxIndex: number = 0;
+
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] > maxVal){
+         maxVal = arr[i];
+         maxIndex = i ;
+        }
+    }
+
+    return maxIndex
+}
+
+console.log(getHighestIndex([5, 21, 12, 21, 8]))
