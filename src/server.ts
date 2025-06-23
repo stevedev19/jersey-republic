f3cf52cf80f71f7c8b1f0afadd218d33d8ed5782
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-06-23 11:52:23 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,3 @@
-/*console.log("EXECUTED")
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-const person: string = "Martin"
-const count: number = 100;*/
-
-// Architectural pattern(suyak, malumotlar oqimini tartibga soluvchi vosita): 
-// MVC = MODEL VIEW CONTROLLER
-// DI = DEPENDENCY INJECTION
-// MVP
-
-// Design pattern(vazifa bajaruvchi qismi, butun backend tizimini malum bulaklarini struktutarasini yechishga xizmat qiladigan pattern): 
-// Middleware, Decorater
