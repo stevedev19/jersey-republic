dd3db09a6a8bec2b8fb490c4233ba87fd011d271
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-06-30 14:04:32 +0900

    feat:develop restaurant login process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -26,3 +26,8 @@ export interface MemberInput {
     memberImage?: String;
     memberPoints?: number;
 }
+
+export interface LoginInput {
+    memberNick: string;
+    memberPassword: string;
+}
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,16 +2,18 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
-/** Restaurant */
-routerAdmin.get('/', restaurantController.goHome); 
-routerAdmin
-.get("/login", restaurantController.getLogin)
- .post("/login", restaurantController.processLogin);
-routerAdmin
-.get("/signup", restaurantController.getSignup)
-.post("/signup", restaurantController.processSignup);
+/* Restaurant */
+routerAdmin.get("/", restaurantController.goHome);
 
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
 
-/** Product */
-/** User */
+routerAdmin
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
+
+/* product */
+/* user */
+
 export default routerAdmin;
