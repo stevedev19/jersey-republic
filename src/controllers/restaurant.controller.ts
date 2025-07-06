a674fe1170335f244afdbdb68dfb31c6bd398f54
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-07-06 17:18:14 +0900

    fix: create sessions on login and signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request,  Response } from "express";
 import{ T } from "../libs/types/common";
 import MemberService from "../models/Member.service"; 
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const memberService = new MemberService();
@@ -34,7 +34,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   }
  };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (req: AdminRequest, res: Response) => {
    try {
     console.log("processSignup");
    
@@ -42,25 +42,31 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
     
-// SESSION AUTHENTICATIONS
 
-    res.send(result);
-  } catch (err) {
+
+req.session.member = result;
+req.session.save(function() {
+  res.send(result);
+   });
+} catch (err) {
     console.log("Error, processSignup", err);
     res.send(err);
   }
  };
 
- restaurantController.processLogin = async (req: Request, res: Response) => {
+ restaurantController.processLogin = async (req: AdminRequest, res: Response) => {
    try {
     console.log("processLogin");
     const input: LoginInput = req.body;
 
     const result =  await memberService.processLogin(input);
 
- // SESSION AUTHENTICATIONS
-    
-    res.send(result);
+ 
+
+ req.session.member = result;
+req.session.save(function() {
+  res.send(result);
+   });
   } catch (err) {
     console.log("Error, processLogin", err);
     res.send(err);  
