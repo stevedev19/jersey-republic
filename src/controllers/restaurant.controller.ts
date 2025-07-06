539dda50053c5672b7acbe0226ddba7faf724a05
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-07-06 17:47:49 +0900

    feat: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import{ T } from "../libs/types/common";
 import MemberService from "../models/Member.service"; 
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -66,12 +67,27 @@ req.session.save(function() {
  req.session.member = result;
 req.session.save(function() {
   res.send(result);
-   });
+   }); 
   } catch (err) {
     console.log("Error, processLogin", err);
     res.send(err);  
   }
  };
+
+ restaurantController.checkAuthSession = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("checkAuthSession");
+    if (req.session?.member) 
+      res.send(`<script>alert("${req.session.member.memberNick}")</script>`);
+      else res.send(`<script>alert("${Message.NOT_AUTHENTICATED}")</script>`);   
+    } catch (err) {
+    console.log("Error, checkAuthSession:", err);
+    res.send(err);
+  }
+};
 
 
  export default restaurantController; 
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -13,6 +13,8 @@ routerAdmin
   .get("/signup", restaurantController.getSignup)
   .post("/signup", restaurantController.processSignup);
 
+routerAdmin.get("/check-me", restaurantController.checkAuthSession);
+
 /* product */
 /* user */
 
