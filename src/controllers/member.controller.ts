8f98efecd21dd8d0375e51021f68281a32825c54
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-08-04 14:57:03 +0900

    feat: develop verify authenticated user credentials

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common"; 
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors, { HttpCode } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../schema/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
 
@@ -45,6 +45,22 @@ memberController.login = async (req: Request, res: Response) => {
     res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error, login", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
+
+memberController.verifyAuth = async (req: Request, res: Response) => {
+try {
+  let member = null;
+  const token = req.cookies["accessToken"];
+  if(token) member = await authService.checkAuth(token);
+  if (!member) 
+    throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+  res.status(HttpCode.OK).json({ member: member });
+} catch (err) {
+ console.log("Error, verifyAuth", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
     }
