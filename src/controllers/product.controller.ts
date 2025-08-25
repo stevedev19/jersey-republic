4a17ccb8b7d25073d8cf6ae655bf52126f4d7e14
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-08-25 23:15:06 +0900

    feat: CORS integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -77,11 +77,12 @@ productController.getProduct = async (req: ExtendedRequest, res: Response) =>{
 ) => {
    try {
     console.log("createNewProduct");
+    
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
- const data: ProductInput = req.body;
-      data.productImages = req.files?.map((ele)=>{
+      const data: ProductInput = req.body;
+      data.productImages = req.files?.map((ele) => {
           return ele.path.replace(/\\/g, "/");
       });
 
